79cd702fff8e375fac2ecc8e263a73cad303292a
Fran García <fgarciadevelop@gmail.com>
2024-04-29 09:39:31 +0200

    feat: added scanner

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { BarcodeFormat } from '@zxing/library';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ZXingScannerModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.sass'
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  formatsEnabled: BarcodeFormat[] = [BarcodeFormat.QR_CODE];
+  scannerEnabled = false;
+
+  handleQrCodeResult(resultString: string) {
+    console.log('Resultado del escaneo QR: ', resultString);
+  }
+
+  enableScanner() {
+    this.scannerEnabled = true;
+  }
 
 }
