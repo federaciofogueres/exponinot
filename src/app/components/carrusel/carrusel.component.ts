05e09381d864de078c10767835f7dbbdb71ef84c
Fran García <fgarciadevelop@gmail.com>
2025-05-21 23:42:42 +0200

    feat: images change

--- src/app/components/carrusel/carrusel.component.ts
+++ src/app/components/carrusel/carrusel.component.ts
@@ -13,12 +13,23 @@ import { SpinnerComponent } from '../spinner/spinner.component';
 export class CarruselComponent {
   private static readonly ONE_HOUR = 3600000;
 
-  images = ['assets/img/sponsors/SP1.png', 'assets/img/sponsors/SP2.png', 'assets/img/sponsors/SP3.png'];
+  images = [
+    'assets/img/publi/SP1.png',
+    'assets/img/publi/SP2.png',
+    'assets/img/publi/SP3.jpg',
+    'assets/img/publi/SP4.png',
+    'assets/img/publi/SP5.png',
+    'assets/img/publi/SP6.jpg',
+    'assets/img/publi/SP7.png',
+    'assets/img/publi/SP8.jpg',
+    'assets/img/publi/SP9.jpg',
+    'assets/img/publi/SP10.jpg'
+  ];
   showCarousel = false;
   slidesView = 0;
   loading = true;
 
-  constructor(private cookieService: CookieService) {}
+  constructor(private cookieService: CookieService) { }
 
   ngOnInit() {
     this.initializeCarousel();
@@ -54,7 +65,7 @@ export class CarruselComponent {
 
   checkCarousel() {
     this.slidesView++;
-    if (this.slidesView >= this.images.length -1) {
+    if (this.slidesView >= this.images.length - 1) {
       this.deactivateCarousel();
     }
   }
